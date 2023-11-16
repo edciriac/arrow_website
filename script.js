03394b22de1766f37772045421ef6f6d5bf8e35d
Edu <edciriac@student.jyu.fi>
2023-11-16 15:57:27 +0200

    Add tooltip if user is inactive

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Show tooltip if user doesn't press on arrow keys for 4 seconds
+let isPressed = false
+const showHelp = () => {
+    if (!isPressed) {
+        document.getElementsByClassName("help")[0].style.animation = "show 5s"
+    }
+}
+setInterval(showHelp, 3000);
+
 // Code for using up and down arrow keys to navigate through the links
 const links = document.getElementsByTagName('a')
 let index = 0
@@ -5,10 +14,12 @@ document.addEventListener('keydown', function(event) {
   if (event.key === 'ArrowUp') {
     index = index== 0? links.length-1: index-1
     links[index].focus() 
+    isPressed = true
   } else if (event.key === 'ArrowDown') {
     index = index== links.length-1? 0: index+1
     links[index].focus()
     console.log('Down arrow key pressed');
+    isPressed = true
   }
 
   // Code for toggling the cursor
@@ -28,4 +39,7 @@ document.addEventListener('keydown', function(event) {
       }
     }
   }
+
 });
+
+
