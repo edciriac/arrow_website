d53193c78d99bc0be63122fe701392fd5b9b4300
Edu <edciriac@student.jyu.fi>
2023-11-20 21:29:41 +0200

    Move tooltip script to index.html

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-
-// Show tooltip if user doesn't press on arrow keys for 4 seconds
-let isPressed = false
-const showHelp = () => {
-    if (!isPressed) {
-        document.getElementsByClassName("help")[0].style.animation = "show 5s"
-    }
-}
-setInterval(showHelp, 3000);
-
 // Code for using up and down arrow keys to navigate through the links
 const links = document.getElementsByClassName("arrow-nav")
 let index = 0 
@@ -48,7 +38,6 @@ document.addEventListener('keydown', function(event) {
     } else if (event.key === 'ArrowDown') {
       index = index== links.length-1? 0: index+1
       links[index].focus()  
-      console.log('Down arrow key pressed');
       isPressed = true
     }
   }
